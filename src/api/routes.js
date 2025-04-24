ef4f97c21f29ec5ac32f4864392fa0d944f1c0a7
DRoDREW <142374284+DRoDREW@users.noreply.github.com>
2025-04-24 21:15:26 +0700

    FINAL

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const productRoutes = require('./components/product/product-route');
-const userRoutes = require('./components/user/user-route');
-const cartRoutes = require('./components/cart/cartRoutes');
+const productRoutes = require('./components/PRODUCTS/product-route');
+const userRoutes = require('./components/USERS/user-route');
+const cartRoutes = require('./components/CARTS/cart-routes');
 
 router.use('/product', productRoutes); 
 router.use('/user', userRoutes);       
